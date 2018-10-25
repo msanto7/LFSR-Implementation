e0b8df03881d186808d497aafaa14b547fe00719
MIKE <mike.santoro42@gmail.com>
2018-10-25 13:53:38 -0400

    intial commit

---
+++ src/LFSR.java
@@ -0,0 +1,36 @@
+
+//Michael Santoro
+//CipherTech Challenge #1
+//10-25-2018
+
+//This Java Program will implement the LFSR based encoding routine
+
+
+
+public class LFSR {
+
+	public static void main(String[] args) {
+		
+		
+		//trying to add to git
+
+		
+		
+		
+		
+		
+		
+		
+		
+	} //Ends main 
+
+	
+	
+	//LFSR Method
+	
+	
+	
+} //Ends Class
+
+
+
